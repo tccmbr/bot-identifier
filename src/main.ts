f239b8fc1ef38ebf43c4858e1a7759462b137da1
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2023-10-26 19:47:40 -0300

    changing default port of web server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3000);
+  app.enableCors({
+    origin: "*",
+  });
+  await app.listen(8080);
 }
 bootstrap();
